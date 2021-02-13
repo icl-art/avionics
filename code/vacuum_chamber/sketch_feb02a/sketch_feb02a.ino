c4776d17c51d6efe1e18b120e56ed928dec8e4d3
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-02-13 09:58:43 +0000

    reconnect every post

--- code/vacuum_chamber/sketch_feb02a/sketch_feb02a.ino
+++ code/vacuum_chamber/sketch_feb02a/sketch_feb02a.ino
@@ -10,9 +10,6 @@ const char *password = "YGF97T9BA8N";
 int status = WL_IDLE_STATUS;
 IPAddress server(192, 168, 8, 104); // Server address
 
-// Initialize the client library
-WiFiClient client;
-
 void setup()
 {
     Serial.begin(115200);
@@ -21,21 +18,34 @@ void setup()
     Serial.println(ssid);
 
     while (!baro.begin()){
-      Serial.println("Couldn't find sensor");
-      delay(1000);
+        Serial.println("Couldn't find sensor");
+        delay(1000);
     }
     
     baro.setSeaPressure(99600);
 
     status = WiFi.begin(ssid, password);
     while(status != WL_CONNECTED){
-      Serial.println("Couldn't get a wifi connection");
-      delay(1000);
-      status = WiFi.begin(ssid, password);
+        Serial.println("Couldn't get a wifi connection");
+        delay(1000);
+        status = WiFi.begin(ssid, password);
     }
 
     Serial.println("Connected to wifi");
-    Serial.println("\nStarting connection...");
+}
+
+void loop()
+{
+    float pascals = baro.getPressure();
+    float tempC = baro.getTemperature();
+    String queryString = "pressure="+String(pascals)+"&temperature="+String(tempC);
+
+    Serial.print(pascals);
+    Serial.println(" Pa");
+    
+    
+    // Initialize the client library
+    WiFiClient client;
 
     while (!client.connect(server, 8080))
     {
@@ -43,27 +53,16 @@ void setup()
         delay(1000);
     }
     Serial.println("connected");
-}
+    // send HTTP header
+    client.println("POST / HTTP/1.1");
+    client.println("Host: localhost");
+    client.println("Content-Length: " + String(queryString.length()));
+    client.println("Connection: close");
+    client.println(); // end HTTP header
 
-void loop()
-{
-  float pascals = baro.getPressure();
-  float tempC = baro.getTemperature();
-  String queryString = "pressure="+String(pascals)+"&temperature="+String(tempC);
+    // send HTTP body
+    client.println(queryString);
+    client.stop()
 
-  Serial.print(pascals);
-  Serial.println(" Pa");
-  
-      // send HTTP header
-  client.println("POST / HTTP/1.1");
-  client.println("Host: localhost");
-  client.println("Content-Length: " + String(queryString.length()));
-  client.println("Connection: close");
-  client.println(); // end HTTP header
-
-  // send HTTP body
-  client.println(queryString);
-  client.println();
-  
-  delay(200);
+    delay(200);
 }
