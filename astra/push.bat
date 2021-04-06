013b7c4588be041bc8b44c732ab427595faec101
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-04-07 00:38:43 +0100

    Create push.bat
    batch script to push new code to pico; uses mpfshell as rshell does not seem to get past connecting stage

---
+++ astra/push.bat
@@ -0,0 +1,17 @@
+@echo off
+cd /d %~dp0
+TITLE Pushing Code
+
+:start
+set /p portsel="Enter COM Port: "
+echo COM%portsel% selected 
+
+set /p fname="Enter file to push, inc extension: "
+echo Pushing %fname%
+CHOICE /C YN /M "Continue "
+IF %ERRORLEVEL% == 1 (GOTO push)
+IF %ERRORLEVEL% == 2 (GOTO start)
+
+:push
+mpfshell -o COM%portsel% -n -c "ls; put %fname% main.py; ls; exit"
+PAUSE
