dc137c47011500692fea2ab9100a389f120e2957
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-04-07 22:36:23 +0100

    switch to threaded approach

--- astra/datacap.py
+++ astra/datacap.py
@@ -1,24 +1,31 @@
 import utime
-from machine import I2C, Pin, Timer
+from machine import I2C, Pin
 import _thread
 from MPL3115A2 import MPL3115A2 as mpl
 import MPU6050
+import serialisation
+
+capture_time = 300
+capture_rate = 10
+delay = int(1000/capture_rate)
+limit = capture_rate*capture_time
+samples = 0
 
 led = Pin(25, Pin.OUT)
 check = Pin(22, Pin.IN, Pin.PULL_UP)
-cap = Timer()
+
 data = []
 
+# initialise barometer
 baro_i2c = I2C(0, scl=Pin(17), sda=Pin(16))
 baro = mpl(baro_i2c, mode=mpl.PRESSURE)
 
+# initialise MPU6050
 mpu = MPU6050.MPU6050(bus = 1, scl = Pin(19), sda = Pin(18))
 mpu.setGResolution(16)
 mpu.setGyroResolution(500)
 
-
-
-def get(timer):
+def get(delay=50):
     global led
     global data
     pressure = baro.pressure()
@@ -26,21 +33,22 @@ def get(timer):
     motion = mpu.readData()
     data = [pressure, temperature, 
     motion.Gx, motion.Gy, motion.Gz,
-    motion.Gyrox, motion.Gyroy, motion.Gyroz]
-    
+    motion.Gyrox, motion.Gyroy, motion.Gyroz]    
     led.toggle()
+    utime.sleep_ms(delay)
+
 
 
 while check.value():
     print("Waiting")
     utime.sleep(1)
 
-cap.init(freq = 10, mode=Timer.PERIODIC, callback = get)
+# initialise log file with 256 bit buffer
+log = serialisation.storage(256, "log")
 
-while True:
-    if check.value():
-        print("Waiting")
-    else:
-        print(data)
-    utime.sleep(1)
-    
+_thread.start_new_thread(get, ())
+
+while samples < limit:
+    log.write(data)
+    samples = samples + 1
+    utime.sleep_ms(delay)
