89ea28eb556ba9a8a35539539e887126d80a0f35
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-04-07 13:44:05 +0100

    ADDED ring_buffer class

---
+++ code/astra/ring_buffer.py
@@ -0,0 +1,27 @@
+#This file implements a ringbuffer that should store the readings for the first n seconds
+
+class RingBuffer:
+    i = 0
+
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.buf = [None] * self.max_size
+    
+    def add(self, reading):
+        if self.i >= self.max_size:
+            self.i = 0
+        self.buf[self.i] = reading
+        self.i += 1
+
+    def __iter__(self):
+        for i in range(self.max_size):
+            yield self.buf[i]
+
+#Example code
+# rb = RingBuffer(4)
+
+# for i in range(10):
+#     rb.add([i, i+1, i+2])
+
+# for i in rb:
+#     print(i)
