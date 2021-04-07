c782e4d9283207c77f7e7c4e770d89464817dceb
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-04-07 22:42:15 +0100

    add variable locking

--- astra/datacap.py
+++ astra/datacap.py
@@ -25,17 +25,26 @@ mpu = MPU6050.MPU6050(bus = 1, scl = Pin(19), sda = Pin(18))
 mpu.setGResolution(16)
 mpu.setGyroResolution(500)
 
-def get(delay=50):
+# create lock to prevent both threads accessing data together
+lock = _thread.allocate_lock()
+
+def get(rest=50):
     global led
     global data
+
     pressure = baro.pressure()
     temperature = baro.temperature()
     motion = mpu.readData()
+
+    # lock data while updating
+    lock.acquire()
     data = [pressure, temperature, 
     motion.Gx, motion.Gy, motion.Gz,
-    motion.Gyrox, motion.Gyroy, motion.Gyroz]    
+    motion.Gyrox, motion.Gyroy, motion.Gyroz]
+    lock.release()    
+
     led.toggle()
-    utime.sleep_ms(delay)
+    utime.sleep_ms(rest)
 
 
 
@@ -49,6 +58,8 @@ log = serialisation.storage(256, "log")
 _thread.start_new_thread(get, ())
 
 while samples < limit:
+    lock.acquire()
     log.write(data)
+    lock.release()
     samples = samples + 1
     utime.sleep_ms(delay)
