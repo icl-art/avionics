314e11410b132aa9cded67b239477f6877d21a61
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-04-07 01:39:36 +0100

    remove frequency change

--- astra/astra.py
+++ astra/astra.py
@@ -474,19 +474,23 @@ tim.init(freq = 2, mode=Timer.PERIODIC, callback=tick)
 utime.sleep(5)
 
 i = 0
-tim.init(freq = 10)
+#tim.init(freq = 10)
 
 f = open("out.csv", "w")
 f.write("T,i\n")
 start = utime.ticks_ms()
+print(start)
 
 while i < 20:
-    tim.init(freq = 2)
-    f.write("{:.4f},{:d}\n".format(utime.ticks_diff(utime.ticks_ms(), start), i))
+    
+    dt = utime.ticks_diff(utime.ticks_ms(), start)
+    line = "%d,%d\n" % (dt, i)
+    print(line)
+    f.write(line)
     i = i + 1
     utime.sleep(1)
 
-tim.init(5)
+#tim.init(freq = 5)
 utime.sleep(5)
 
 f.close()
