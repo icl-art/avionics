c834a4e3f80a3158bfb14e5a84162cb4f2bf87b2
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-04-08 21:11:17 +0100

    add micropython copy implementation

--- astra/compile.bat
+++ astra/compile.bat
@@ -5,7 +5,16 @@ ECHO Compiling Altimeter
 mpy-cross -O[4] -march=armv7m MPL3115A2.py
 
 ECHO Compiling MPU
-mpy-cross -O[4] -march=armv7m MPU6050.py 
+mpy-cross -O[4] -march=armv7m MPU6050.py
+
+ECHO Compiling Ring Buffer
+mpy-cross -O[4] -march=armv7m ring_buffer.mpy
+
+ECHO Compiling Serialisation
+mpy-cross -O[4] -march=armv7m serialisation.py
+
+ECHO Compiling copy
+mpy-cross -O[4] -march=armv7m copy.py 
 
 ECHO Complete
 PAUSE
