e8277129219c39540d935ce1aecb8519686155d9
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-04-11 22:30:59 +0100

    fix plots

--- data_processing/astra.m
+++ data_processing/astra.m
@@ -4,6 +4,8 @@ col1 = '#2978a0';
 col2 = '#BA1200';
 col3 = '#3EC300';
 col4 = '#3C153B';
+lw = 1;
+
 G = 9.8065;
 
 data = readmatrix('astra.csv');
@@ -20,55 +22,54 @@ g_x = data(:, 7);
 g_y = data(:, 8);
 g_z = data(:, 9);
 
+% convert from ms to s
 t = t/1000;
 
 figure
 hold on
-plot(t, temp, 'LineWidth', 1, 'Color', col1)
+plot(t, acc_x, 'LineWidth', lw, 'Color', col1)
+plot(t, acc_y, 'LineWidth', lw, 'Color', col2)
+plot(t, acc_z, 'LineWidth', lw, 'Color', col3)
+plot(t, acc_magnitude, 'LineWidth', 1, 'Color', col4)
+title('Acceleration');
+xlabel('Time (s)')
+ylabel('Acceleration (m/s^2)')
+legend('x', 'y', 'z', 'Magnitude')
+xlim([(min(t)-1), (max(t)+1)])
+
+figure
+hold on
+plot(t, g_x, 'LineWidth', lw, 'Color', col1)
+plot(t, g_y, 'LineWidth', lw, 'Color', col2)
+plot(t, g_z, 'LineWidth', lw, 'Color', col3)
+title('Angular Velocity');
+xlabel('Time (s)')
+ylabel('Rotation (deg/s)')
+legend('x', 'y', 'z')
+xlim([(min(t)-1), (max(t)+1)])
+hold off
+
+figure
+plot(t, pressure, 'LineWidth', lw, 'Color', col1)
+title('Pressure')
+xlabel('Time(s)')
+ylabel('Pressure (Pa)')
+xlim([(min(t)-1), (max(t)+1)])
+
+height = atmospalt(pressure);
+figure
+plot(t, pressure, 'LineWidth', lw, 'Color', col1)
+title('Altitude ASL')
+xlabel('Time(s)')
+ylabel('Altitude (m)')
+xlim([(min(t)-1), (max(t)+1)])
+
+figure
+hold on
+plot(t, temp, 'LineWidth', lw, 'Color', col1)
 title('Temperature');
 xlabel('Time(s)');
 ylabel('Temperature (C)');
 xlim([(min(t)-1), (max(t)+1)])
 hold off
-
-figure
-title('Acceleration');
-xlabel('Time (s)')
-ylabel('Acceleration (m/s^2)')
-hold on
-plot(t, acc_x, 'LineWidth', 1, 'Color', col1)
-plot(t, acc_y, 'LineWidth', 1, 'Color', col2)
-plot(t, acc_z, 'LineWidth', 1, 'Color', col3)
-plot(t, acc_magnitude, 'LineWidth', 1, 'Color', col4)
-legend('x', 'y', 'z', 'Magnitude')
-xlim([(min(t)-1), (max(t)+1)])
-
-figure
-title('Angular Velocity');
-xlabel('Time (s)')
-ylabel('Gyro (rad/s)')
-hold on
-plot(t, g_x, 'LineWidth', 1, 'Color', col1)
-plot(t, g_y, 'LineWidth', 1, 'Color', col2)
-plot(t, g_z, 'LineWidth', 1, 'Color', col3)
-legend('x', 'y', 'z')
-xlim([(min(t)-1), (max(t)+1)])
-hold off
-
-height = atmospalt(pressure);
-
-figure
-title('Pressure/Altitude Plot')
-xlabel('Time(s)')
-
-yyaxis left
-plot(t, pressure, 'LineWidth', 1, 'Color', col1)
-ylabel('Pressure (Pa)')
-
-yyaxis right
-plot(t, height, 'LineWidth', 1, 'Color', col2)
-ylabel("Altitude ASL (m)")
-
-xlim([(min(t)-1), (max(t)+1)])
-
 
