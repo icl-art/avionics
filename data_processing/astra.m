62129991fb3b0483a9eac41f69572000560c441d
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-04-13 00:23:57 +0100

    plot pnut data

--- data_processing/astra.m
+++ data_processing/astra.m
@@ -32,7 +32,7 @@ plot(t, acc_x, 'LineWidth', lw, 'Color', col1)
 plot(t, acc_y, 'LineWidth', lw, 'Color', col2)
 plot(t, acc_z, 'LineWidth', lw, 'Color', col3)
 plot(t, acc_magnitude, 'LineWidth', 1, 'Color', col4)
-title('Acceleration');
+title('Acceleration - 💩📦');
 xlabel('Time (s)')
 ylabel('Acceleration (m/s^2)')
 legend('x', 'y', 'z', 'Magnitude', 'Location', 'best')
@@ -44,7 +44,7 @@ hold on
 plot(t, g_x, 'LineWidth', lw, 'Color', col1)
 plot(t, g_y, 'LineWidth', lw, 'Color', col2)
 plot(t, g_z, 'LineWidth', lw, 'Color', col3)
-title('Angular Velocity');
+title('Angular Velocity - 💩📦');
 xlabel('Time (s)')
 ylabel('Rotation (deg/s)')
 legend('x', 'y', 'z', 'Location', 'best')
@@ -55,30 +55,61 @@ figure
 plot(t, pressure, 'LineWidth', lw, 'Color', col1)
 title('Pressure')
 xlabel('Time(s)')
-ylabel('Pressure (Pa)')
+ylabel('Pressure (Pa) - 💩📦')
 xlim([(min(t)-1), (max(t)+1)])
 exportgraphics(gcf, sprintf('%s_pressure.png', launch), 'Resolution', 600);
 
 height = atmospalt(pressure);
 figure
-plot(t, height, 'LineWidth', lw, 'Color', col1)
-title('Altitude ASL')
+plot(t, height, 'LineWidth', lw, 'Color', col2)
+title('Altitude ASL - 💩📦')
 xlabel('Time(s)')
 ylabel('Altitude (m)')
 xlim([(min(t)-1), (max(t)+1)])
 exportgraphics(gcf, sprintf('%s_altitude.png', launch), 'Resolution', 600);
 
 figure
-plot(t, temp, 'LineWidth', lw, 'Color', col1)
-title('Temperature');
+plot(t, temp, 'LineWidth', lw, 'Color', col3)
+title('Temperature - 💩📦');
 xlabel('Time(s)');
 ylabel('Temperature (C)');
 xlim([(min(t)-1), (max(t)+1)])
 exportgraphics(gcf, sprintf('%s_temperature.png', launch), 'Resolution', 600);
 
 figure
-plot(t, 'LineWidth', lw, 'Color', col1)
-title('Data Recording')
+plot(t, 'LineWidth', lw, 'Color', col4)
+title('Data Recording - 💩📦')
 xlabel('Data Point');
 ylabel('Time Elapsed')
 exportgraphics(gcf, sprintf('%s_recording.png', launch), 'Resolution', 600);
+
+pnut = importdata(sprintf('%s.pf2', launch), ',', 11).data;
+
+t = pnut(:, 1);
+altitude = pnut(:, 2)./3.281;
+vel = pnut(:, 3)./3.281;
+temp = pnut(:, 4);
+
+figure
+plot(t, altitude, 'LineWidth', lw, 'Color', col1)
+title('Altitude AGL - Pnut')
+xlabel('Time(s)')
+ylabel('Altitude (m)')
+xlim([(min(t)-1), (max(t)+1)])
+exportgraphics(gcf, sprintf('%s_altitude_pnut.png', launch), 'Resolution', 600);
+
+figure
+plot(t, vel, 'LineWidth', lw, 'Color', col2)
+title('Velocity - Pnut')
+xlabel('Time(s)')
+ylabel('Velocity (m/s)')
+xlim([(min(t)-1), (max(t)+1)])
+exportgraphics(gcf, sprintf('%s_velocity_pnut.png', launch), 'Resolution', 600);
+
+figure
+plot(t, temp, 'LineWidth', lw, 'Color', col3)
+title('Temperature - Pnut')
+xlabel('Time(s)')
+ylabel('Velocity (m/s)')
+xlim([(min(t)-1), (max(t)+1)])
+exportgraphics(gcf, sprintf('%s_temperature_pnut.png', launch), 'Resolution', 600);
