3d7b0d344f95202cff7723ce0fa3d6702c053b9b
Akshat Tripathi <at619@ic.ac.uk>
2021-05-24 16:39:21 +0100

    Updated tests with new storage behaviour

--- astra/tests/test_storage.py
+++ astra/tests/test_storage.py
@@ -1,13 +1,16 @@
 import unittest
 from unittest.mock import Mock
+import struct
 
-from src.serialisation import storage
+from src.serialisation import storage, NORMAL, RING
 
 class TestStorage(unittest.TestCase):
-    
+
+    #Normal Mode tests
     def test_data_not_dumped_when_not_full(self):
         file = Mock()
-        st = storage(5*8, file)
+        st = storage(2*5*4, 0, file)
+        st.set_mode(NORMAL)
 
         for _ in range(4):
             st.write([1.0, 2.0])
@@ -15,7 +18,8 @@ class TestStorage(unittest.TestCase):
  
     def test_data_dumped_on_full(self):
         file = Mock()
-        st = storage(5*8, file)
+        st = storage(2*5*4, 0, file)
+        st.set_mode(NORMAL)
 
         for _ in range(6):
             st.write([1.0, 2.0])
@@ -24,9 +28,46 @@ class TestStorage(unittest.TestCase):
 
     def test_data_dumped_on_close(self):
         file = Mock()
-        st = storage(5*8, file)
+        st = storage(2*5*4, 0, file)
+        st.set_mode(NORMAL)
+
         st.write([1.0, 2.0])
         st.close()
 
         file.write.assert_called_once()
         file.close.assert_called_once()
+
+    #Ring Mode tests
+    #Note normal python runs with 64 bit floats rather than 32 bit floats that micropython uses
+    def test_adding_below_max_doesnt_overrite(self):
+        file = Mock()
+        st = storage(0, 5*4, file)
+        st.set_mode(RING)
+
+        buf = bytearray(5*4)
+        for i in range(3):
+            st.write([i])
+            buf[4*i:] = struct.pack("f", i)
+        
+        st.flush()
+
+        file.write.assert_called_once_with(buf)
+
+    def test_adding_above_max_overrites_data(self):
+        file = Mock()
+        st = storage(0, 5*4, file)
+        st.set_mode(RING)
+
+        buf = bytearray(5*4)
+        for i in range(6):
+            st.write([i])
+            buf[4*i:] = struct.pack("f", i)
+        
+        st.flush()
+
+        try:
+            file.write.assert_called_once_with(buf)
+        except AssertionError:
+            return
+
+        raise Exception("Expected buffer to overflow")
