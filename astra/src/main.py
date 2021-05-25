962ffd0b120f49bfe1609039f53cbfb43f6ca18b
Akshat Tripathi <at619@ic.ac.uk>
2021-05-25 18:38:47 +0100

    Refactored state machine transitions back into states

--- astra/src/main.py
+++ astra/src/main.py
@@ -2,7 +2,7 @@ import utime
 from machine import Pin
 
 from .serialisation import storage, RING, NORMAL
-from .state_machine import state, state_machine
+from .state_machine import state, run
 from .sensors import sensors
 
 CAPTURE_RATE = 20
@@ -15,23 +15,24 @@ def indicate(msg):
         print(msg)
     led.toggle()
 
-class idle(state):
-    NEXT_STATE = 1
 
+sens = sensors(indicate=indicate)
+log = storage(STORAGE_BUFFER_SIZE, "log.bin")
+
+class idle(state):
     def run(self) -> int:
         check = Pin(22, Pin.IN, Pin.PULL_UP)
 
         while check.value():
             utime.sleep(1)
             indicate("Waiting")
-        return self.NEXT_STATE
+        return preflight(log, sens)
     
 class preflight(state):
     NEXT_STATE = 1
     ACCEL_THRESH = 4*4
     DELAY = MILLIS // (2 * CAPTURE_RATE)
 
-    #TODO: Add a ring buffer mode to storage
     def __init__(self, buffer: storage, sensors: sensors):
         self.buffer = buffer
         self.sensors = sensors
@@ -44,9 +45,9 @@ class preflight(state):
             data = self.sensors.get()
             x, y, z = data[3], data[4], data[5]
             magnitude = x*x + y*y + z*z
-            self.buffer._write_normal(data)
+            self.buffer.write(data)
             utime.sleep_ms(self.DELAY)
-        return self.NEXT_STATE
+        return flight(log, sens)
 
 class flight(state):
     NEXT_STATE = 1
@@ -63,12 +64,12 @@ class flight(state):
             try:
                 indicate()
                 data = self.sensors.get()
-                self.buffer._write_normal()
+                self.buffer.write(data)
                 utime.sleep_ms(self.DELAY)
             except:
                 break
         indicate("Flight done")
-        return self.NEXT_STATE
+        return postflight()
 
 class postflight(state):
     def run(self) -> int:
@@ -76,16 +77,8 @@ class postflight(state):
             indicate("Awaiting recovery")
             utime.sleep(1)
 
-sens = sensors(indicate=indicate)
-log = storage(STORAGE_BUFFER_SIZE, "log.bin")
-
-machine = state_machine([
-    idle(),
-    preflight(log, sens),
-    flight(log, sens),
-    postflight()
-])
-
 utime.sleep(2)
 
-machine.run()
+state = idle()
+while state:
+    state = state.run()
--- astra/src/state_machine.py
+++ astra/src/state_machine.py
@@ -1,32 +1,5 @@
-CONTINUE = -1
-
 from abc import abstractmethod
 class state:
     @abstractmethod
-    def run(self) -> int:
-        pass
-class state_machine:
-    def __init__(self, states: list[state], start_state = 0, sequential=True):
-        if len(states) == 0:
-            raise UserWarning("State machine created with 0 states")
-        if len(states) < start_state < 0:
-            raise ValueError("Cannot create a state machine with start state {start_state}")
-
-        self.states = states
-        self.start_state = start_state
-        self.transition_table = {i:set([i+1]) for i in range(len(states)-1)} if sequential else {}
-
-    def add_transition(self, start, end):
-        self.transition_table.setdefault(start, set([end])).add(end)
-
     def run(self):
-        state = self.states[self.start_state]
-        curr = self.start_state
-        while True:
-            next_state = state.run()
-            if next_state != CONTINUE:
-                if next_state in self.transition_table.get(curr):
-                    curr = next_state
-                    state = self.states[curr]
-                else:
-                    break
+        pass
