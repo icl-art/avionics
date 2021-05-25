b204f90e281286631b992d92c01e66847755d370
Akshat Tripathi <at619@ic.ac.uk>
2021-05-25 20:46:14 +0100

    Added tests for state machines

--- astra/src/main.py
+++ astra/src/main.py
@@ -1,81 +1,14 @@
 import utime
-from machine import Pin
 
 from .serialisation import storage, RING, NORMAL
-from .state_machine import state
+from .state_machine import state, idle
 from .sensors import sensors
 
-CAPTURE_RATE = 20
-MILLIS = 1000
 STORAGE_BUFFER_SIZE = 256
+RING_BUFFER_SIZE = 80
 
-led = Pin(25, Pin.OUT)
-def indicate(msg):
-    if msg:
-        print(msg)
-    led.toggle()
-
-
-sens = sensors(indicate=indicate)
-log = storage(STORAGE_BUFFER_SIZE, "log.bin")
-
-class idle(state):
-    def run(self) -> int:
-        check = Pin(22, Pin.IN, Pin.PULL_UP)
-
-        while check.value():
-            utime.sleep(1)
-            indicate("Waiting")
-        return preflight(log, sens)
-    
-class preflight(state):
-    NEXT_STATE = 1
-    ACCEL_THRESH = 4*4
-    DELAY = MILLIS // (2 * CAPTURE_RATE)
-
-    def __init__(self, buffer: storage, sensors: sensors):
-        self.buffer = buffer
-        self.sensors = sensors
-
-    def run(self) -> int:
-        self.log.set_mode(RING)
-        magnitude = 0
-        while magnitude < self.ACCEL_THRESH:
-            indicate(None)
-            data = self.sensors.get()
-            x, y, z = data[3], data[4], data[5] #This isn't great - since preflight should not know the data format
-            magnitude = x*x + y*y + z*z
-            self.buffer.write(data)
-            utime.sleep_ms(self.DELAY)
-        return flight(log, sens)
-
-class flight(state):
-    NEXT_STATE = 1
-    DELAY = MILLIS // CAPTURE_RATE
-
-    def __init__(self, buffer: storage, sensors: sensors):
-        self.buffer = buffer
-        self.sensors = sensors
-
-    def run(self) -> int:
-        self.log.set_mode(NORMAL)
-        #TODO: Work out when to stop recording
-        while True:
-            try:
-                indicate()
-                data = self.sensors.get()
-                self.buffer.write(data)
-                utime.sleep_ms(self.DELAY)
-            except:
-                break
-        indicate("Flight done")
-        return postflight()
-
-class postflight(state):
-    def run(self) -> int:
-        while True:
-            indicate("Awaiting recovery")
-            utime.sleep(1)
+sens = sensors()
+log = storage(STORAGE_BUFFER_SIZE, RING_BUFFER_SIZE, "log.bin")
 
 utime.sleep(2)
 
--- astra/tests/test_states.py
+++ astra/tests/test_states.py
@@ -1,26 +1,82 @@
 
 import unittest
 from unittest.mock import Mock
+from random import random
+import timeout_decorator
+import sys
+import io
 
-# from src.main import preflight, flight, postflight
+from src.state_machine import preflight, flight, postflight
+from src.serialisation import RING, NORMAL
 
 class TestStates(unittest.TestCase):
     
     #Preflight tests
     def test_preflight_uses_ring_buffer(self):
-        ...
+        buffer = Mock()
+        sensors = Mock()
+        sensors.get = Mock(return_value = [100]*9)
+        state = preflight(buffer, sensors)
+        state.run()
+
+        buffer.set_mode.assert_called_once_with(RING)
+
 
     def test_preflight_ends_when_acceleration_gt_ACCEL_THRESH(self):
-        ...
+        buffer = Mock()
+        sensors = Mock()
+        state = preflight(buffer, sensors)
+        
+        n = 10
+        data = []
+        for _ in range(n-1):
+            x, y, z = random(), random(), random()
+            while x*x + y*y + z*z > state.ACCEL_THRESH:
+                x, y, z = random(), random(), random()
+            data.append([0, 0, 0, x, y, z, 0, 0, 0])
+        root_mag = (state.ACCEL_THRESH/3) ** 0.5
+        data.append([0, 0, 0, root_mag, root_mag, root_mag, 0, 0, 0])
+
+        sensors.get = Mock(side_effect=data)
+        next_state = state.run()
+
+        assert sensors.get.call_count == n
+        assert buffer.write.call_count == n
+        assert isinstance(next_state, flight)
+
     
     #Flight tests
     def test_flight_uses_normal_buffer(self):
-        ...
+        buffer = Mock()
+        sensors = Mock()
+        state = flight(buffer, sensors)
+
+        sensors.get = Mock(return_value = [100]*9)
+        state.run()
+
+        buffer.set_mode.assert_called_once_with(NORMAL)
+
     
     #Note this is subject to change
-    def test_flight_uses_ends_when_buffer_full(self):
+    def test_flight_uses_postflight_when_buffer_full(self):
         ...
     
+    @timeout_decorator.timeout(0.01)
+    def run_postflight(self, state):
+        #Disables printing for this code
+        with unittest.mock.patch('sys.stdout', new = io.StringIO()):
+            state.run()
+
     #Postflight tests
     def test_nothing_happens_in_postflight(self):
-        ...
+        buffer = Mock()
+        sensors = Mock()
+        state = postflight()
+
+        try:
+            self.run_postflight(state)
+        except timeout_decorator.timeout_decorator.TimeoutError:
+            pass
+
+        sensors.get.assert_not_called()
+        buffer.write.get.assert_not_called()
