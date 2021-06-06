25fa9a8a7d2b4c68784e4a5c04bc8e17ec795fe9
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-06-06 10:14:29 +0100

    Fixed test script - now exits with failure exit code

--- astra/tools/test.sh
+++ astra/tools/test.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 #Enable TEST_MODE then test
-sed -i 's/TEST_MODE = False/TEST_MODE = True/' src/external.py
-for test in $(ls tests | grep -E "(.py)$" | cut -f 1 -d '.'); do python3 -m unittest tests.$test; done
+sed -i 's/TEST_MODE = False/TEST_MODE = True/' src/external.py;
+exit_code=0
+for test in $(ls tests | grep -E "(.py)$" | cut -f 1 -d '.')
+do
+    python3 -m unittest tests.$test || exit_code=$?;
+done
 sed -i 's/TEST_MODE = True/TEST_MODE = False/' src/external.py
+exit $exit_code
