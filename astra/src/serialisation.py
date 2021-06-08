2e11190d521a3bdf1d1ad56f0e4589d46e12b020
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-06-08 17:29:44 +0100

    Removed multicore flush

--- astra/src/serialisation.py
+++ astra/src/serialisation.py
@@ -14,19 +14,13 @@ class storage:
         self.ring_buffer_size = ring_buffer_size
         self.buffer = bytearray(max_buffer_size)
         self.file = file
-        self.write = self._write_normal
-
-        self.reserve_buffer = bytearray(max_buffer_size)
-        self.buffer_lock = _thread.allocate_lock()
-        self.flush_sema = _thread.allocate_lock()
-        self.flush_sema.acquire()
-        _thread.start_new_thread(self.thread_1, ())
+        self.write = self._write_normal   
 
     #Readings is a tuple of the readings
     #Note a max_buffer_size must be divisible by the size of the readings
     def _write_normal(self, readings):
         if self.buffer_size >= self.max_buffer_size:
-            self.flush_async()
+            self.flush()
         
         for val in readings:
             self.buffer[self.buffer_size:] = struct.pack("f", val)
@@ -42,19 +36,6 @@ class storage:
     def flush(self):
         self.file.write(self.buffer)
         self.buffer_size = 0
-        
-    def flush_async(self):
-        with self.buffer_lock:
-            self.buffer, self.reserve_buffer = self.reserve_buffer, self.buffer
-            self.buffer_size = 0
-        self.flush_sema.release()
-
-    def thread_1(self):
-        while True:
-            self.flush_sema.acquire()
-            with self.buffer_lock:
-                self.file.write(self.reserve_buffer)
-
 
     def close(self):
         self.flush()
@@ -66,9 +47,3 @@ class storage:
             self.write = self._write_normal
         elif mode == RING:
             self.write = self._write_ring
-
-#Example code
-# s = storage(1, open("test", "wb"))
-# s.write([1.23, 4.56])
-# s.write([7.89, 1234.124])
-# s.close()
