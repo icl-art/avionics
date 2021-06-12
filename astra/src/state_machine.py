c1616d2b50672f542602fce2e7602d85aa908a31
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-06-12 11:09:30 +0100

    Replaced preflight stage with a 15 minute wait

--- astra/src/state_machine.py
+++ astra/src/state_machine.py
@@ -14,7 +14,7 @@ MILLIS = 1000
 
 DATA_SIZE = 9 * 4
 STORAGE_BUFFER_SIZE = 50 * CAPTURE_RATE #50 seconds
-RING_BUFFER_SIZE = 20 * 2 * CAPTURE_RATE #20 seconds
+PREFLIGHT_DELAY = 15 * 60 # 15 minutes
 
 FLIGHT_TIME = 5 * 60 * CAPTURE_RATE
 
@@ -37,33 +37,11 @@ class idle(state):
         n_logs = len(list(filter(lambda s: "log" in s, listdir())))
         if n_logs != 0:
             buzzer.playsong("miichannel.txt")
-        buffer = storage(STORAGE_BUFFER_SIZE*DATA_SIZE,
-                         RING_BUFFER_SIZE*DATA_SIZE, open("log{}.bin".format(n_logs), "wb"))
+        buffer = storage(STORAGE_BUFFER_SIZE*DATA_SIZE, 0, open("log{}.bin".format(n_logs), "wb"))
         buzzer.playsong("nokia.txt")
-        return preflight(buffer, sensor)
-    
-class preflight(state):
-    NEXT_STATE = 1
-    ACCEL_THRESH = 4*4
-    DELAY = MILLIS // (2 * CAPTURE_RATE)
-
-    def __init__(self, buffer, sensors):
-        self.buffer = buffer
-        self.sensors = sensors
-
-    def run(self) -> state:
-        self.buffer.set_mode(RING)
-        magnitude = 0
-        while magnitude < self.ACCEL_THRESH:
-            data = self.sensors.get()
-            x, y, z = data[3], data[4], data[5] #This isn't great - since preflight should not know the data format
-            magnitude = x*x + y*y + z*z
-            self.buffer.write(data)
-            utime.sleep_ms(self.DELAY)
-        return flight(self.buffer, self.sensors)
-
+        utime.sleep(PREFLIGHT_DELAY)
+        return flight(buffer, sensor)
 class flight(state):
-    NEXT_STATE = 1
     DELAY = MILLIS // CAPTURE_RATE
 
     def __init__(self, buffer, sensors):
@@ -72,13 +50,13 @@ class flight(state):
 
     def run(self) -> state:
         self.buffer.set_mode(NORMAL)
+        buzzer.playsong("nokia.txt")
         #TODO: Work out when to stop recording
         i = 0
         while i < FLIGHT_TIME:
             try:
                 indicate(None)
                 data = self.sensors.get()
-                print(i, data)
                 self.buffer.write(data)
                 utime.sleep_ms(self.DELAY)
             except Exception as e:
@@ -87,11 +65,9 @@ class flight(state):
             i += 1
         indicate("Flight done")
         self.buffer.close()
-        buzzer.playsong("mario.txt")
         return postflight()
 
 class postflight(state):
     def run(self) -> int:
         while True:
-            indicate("Awaiting recovery")
-            utime.sleep(1)
+            buzzer.playsong("mario.txt")
