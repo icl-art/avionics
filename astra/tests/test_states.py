f52f720cf4c54b339c6ea243b6a5d9c0081302d6
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-06-13 19:44:12 +0100

    Removed preflight stage from tests

--- astra/tests/test_states.py
+++ astra/tests/test_states.py
@@ -1,7 +1,6 @@
 
 import unittest
 from unittest.mock import Mock
-from random import random
 import timeout_decorator
 import io
 
@@ -9,44 +8,10 @@ import io
 from sys import path
 path[0] += "/src"
 
-from state_machine import preflight, flight, postflight
-from serialisation import RING, NORMAL
+from state_machine import flight, postflight
+from serialisation import NORMAL
 
 class TestStates(unittest.TestCase):
-    
-    #Preflight tests
-    def test_preflight_uses_ring_buffer(self):
-        buffer = Mock()
-        sensors = Mock()
-        sensors.get = Mock(return_value = [100]*9)
-        state = preflight(buffer, sensors)
-        state.run()
-
-        buffer.set_mode.assert_called_once_with(RING)
-
-
-    def test_preflight_ends_when_acceleration_gt_ACCEL_THRESH(self):
-        buffer = Mock()
-        sensors = Mock()
-        state = preflight(buffer, sensors)
-        
-        n = 10
-        data = []
-        for _ in range(n-1):
-            x, y, z = random(), random(), random()
-            while x*x + y*y + z*z > state.ACCEL_THRESH:
-                x, y, z = random(), random(), random()
-            data.append([0, 0, 0, x, y, z, 0, 0, 0])
-        root_mag = (state.ACCEL_THRESH/3) ** 0.5
-        data.append([0, 0, 0, root_mag, root_mag, root_mag, 0, 0, 0])
-
-        sensors.get = Mock(side_effect=data)
-        next_state = state.run()
-
-        assert sensors.get.call_count == n
-        assert buffer.write.call_count == n
-        assert isinstance(next_state, flight)
-
     
     #Flight tests
     def test_flight_uses_normal_buffer(self):
